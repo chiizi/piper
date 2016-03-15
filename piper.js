7d6d6d5251ee6b67365fbb92ad234a159e5fd0ac
Hans Felde <m33pfish@gmail.com>
2016-03-15 10:09:48 -0500

    Oh crap my battery

--- piper.js
+++ piper.js
@@ -1,3 +1,26 @@
 const piper = function(c) {
+  const ops = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "--": n => -n,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+    "**": (a, b) => Math.pow(a, b),
+    "pi": () => Math.PI
+  }
   
+  const stack = []
+  
+  c = c.split(/\s+/g)
+  c.forEach(t => {
+    if (parseFloat(c) !== NaN) {
+      stack.push(parseFloat(c))
+    } else {
+      var args = []
+      for (var i = 0; i < ops[t].length; i++)
+        args.push(stack.pop())
+      stack.push(ops[t](...args))
+    }
+  })
+  return stack[stack.length - 1]
 }
